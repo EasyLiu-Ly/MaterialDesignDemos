150dc0af15f605f38791f3b4bfcdbe92e7c49c18
EasyLiu-Ly <ly_eyl@163.com>
2016-07-14 13:29:06 +0800

    add project ToolBarDemo

---
+++ MaterialDesignDemo/toolbardemo/src/main/java/com/easyliu/demo/toolbardemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.easyliu.demo.toolbardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                return true;
+            case R.id.action_refresh:
+                return true;
+            case R.id.action_search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
